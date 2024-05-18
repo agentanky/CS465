f4c5765eee72c02a001c60209acae1849d55bd5d
Ankur Tandan <tandan@gmail.com>
2024-05-18 11:34:47 -0700

    removed console logs for logging

--- travlr/travlr/app_server/controllers/travel.js
+++ travlr/travlr/app_server/controllers/travel.js
@@ -1,8 +1,8 @@
-console.log('Travel controller loaded');  // Add this log
+
 
 /* Get travel view */
 const travel = (req, res, next) => {
-  console.log('Rendering travel view');  // Add this log
+
   res.render('travel', { title: "Travlr Getaways" });
 };
 
@@ -10,4 +10,4 @@ module.exports = {
   travel
 };
 
-console.log('Travel controller exported');  // Add this log
+
